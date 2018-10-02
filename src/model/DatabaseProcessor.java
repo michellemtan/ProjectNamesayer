aee86373adba0f8627edd89f405a1997ecdc9fff
rowanh22 <rowanhart22@gmail.com>
2018-10-02 23:08:18 +1300

    Processor creates folders & deals with chen=Chen

--- src/model/DatabaseProcessor.java
+++ src/model/DatabaseProcessor.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseProcessor {
 
@@ -25,12 +27,46 @@ public class DatabaseProcessor {
                     new File(pathToDB + "/" + name).mkdir();
                     //Trim audio into newly created folder
                     String command = "ffmpeg -y -i " + file.getPath() + " -af silenceremove=1:0:-35dB " + pathToDB + "/" + name + "/" + file.getName();
-                    System.out.println(command);
                     trimAudio(command);
                     file.renameTo(new File(pathToDB + "/uncut_files/" + file.getName()));
                 }
             }
         }
+
+        //Code to deal with same names, case insensitive (chen = Chen)
+        File[] namesListing = dir.listFiles();
+        List<String> names = new ArrayList<>();
+        //Create list of strings of names
+        for(File file : namesListing) {
+            names.add(file.getName());
+        }
+        //Iterate through names, and if there's a match then move into new upCased directory and delete old one
+        for(String name : names) {
+            if(containsCaseInsensitive(name, names)) {
+                String upCased = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+                new File(pathToDB + "/" + upCased).mkdir();
+                File nameDir = new File(pathToDB + "/" + name);
+                File[] subFiles = nameDir.listFiles();
+                for(File file : subFiles) {
+                    file.renameTo(new File(pathToDB + "/" + upCased + "/" + file.getName()));
+                }
+                nameDir.delete();
+            }
+        }
+
+    }
+
+    /**@param toCompare the String that we're checking is in the supplied list
+     * @param list the supplied list
+     * @return boolean value indicating if the String is in the List
+     */
+    private boolean containsCaseInsensitive(String toCompare, List<String> list){
+        for (String string : list){
+            if (string.equalsIgnoreCase(toCompare) && !string.equals(toCompare)){
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
