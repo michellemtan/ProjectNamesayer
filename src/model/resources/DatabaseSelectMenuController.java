fc1297013caf6ce9cc1e4a52ac4338dd13bd7664
rowanh22 <rowanhart22@gmail.com>
2018-10-02 13:32:39 +1300

    Added choose db feature

--- src/model/resources/DatabaseSelectMenuController.java
+++ src/model/resources/DatabaseSelectMenuController.java
@@ -1,50 +1,77 @@
 package model.resources;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.input.MouseEvent;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
 
 public class DatabaseSelectMenuController {
 
-    @FXML
-    private ListView<?> dbListview;
+    @FXML private ListView<String> dbListView;
+    @FXML private Button backButton;
+    @FXML private Button namesBtn;
+    @FXML private Button loadBtn;
+    private Preferences dbPref = Preferences.userRoot();
 
-    @FXML
-    private Button deleteBtn;
+    public void initialize() {
+        //Create list of keys and add to list view if valid
+        String[] prefKeys = new String[0];
+        try {
+            prefKeys = dbPref.keys();
 
-    @FXML
-    private Button continueBtn;
+        } catch (BackingStoreException e) {
+        }
+        for(String key : prefKeys) {
+            if(key.startsWith("/")) {
+                dbListView.getItems().add(dbPref.get(key, key));
+            }
+        }
 
-    @FXML
-    private Button backButton;
+        //Disable button and enable if 1 list item selected
+        namesBtn.setDisable(true);
+        loadBtn.setDisable(true);
+        dbListView.getSelectionModel().selectedItemProperty().addListener((obs, old, newI) -> {
+            if(dbListView.getSelectionModel().getSelectedItems().size() == 1) {
+                namesBtn.setDisable(false);
+                loadBtn.setDisable(false);
+            }
+        });
 
-    @FXML
-    void addBtnPressed(ActionEvent event) {
+        //dbPref.clear();
+    }
 
+    public void namesBtnPressed() {
+        System.out.println("names");
+    }
+
+    public void loadBtnPressed() {
+        System.out.println("load");
     }
 
     @FXML
-    void backButtonClicked(MouseEvent event) throws IOException {
+    void backButtonClicked() throws IOException {
         Scene scene = SetUp.getInstance().startMenu;
         Stage window = (Stage) backButton.getScene().getWindow();
         window.setScene(scene);
-
     }
 
-    @FXML
-    void continueBtnPressed(ActionEvent event) {
+    public void addBtnPressed() {
+        DirectoryChooser dc = new DirectoryChooser();
+        dc.setTitle("Choose database folder");
+        Stage dcStage = new Stage();
+        File selectedDirectory = dc.showDialog(dcStage);
 
+        if (selectedDirectory != null) {
+            //Add selected file to preferences to be saved
+            dbListView.getItems().add(selectedDirectory.getPath());
+            dbPref.put(selectedDirectory.getPath(), selectedDirectory.getPath());
+        }
     }
-
-    @FXML
-    void deleteBtnPressed(MouseEvent event) {
-
-    }
-
 }
