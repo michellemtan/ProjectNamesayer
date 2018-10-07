08a338b0699b1a1f49da22c82ec06c373cfb5f55
Michelle Tan <michellemtan@outlook.com>
2018-10-07 14:19:34 +1300

    Adding list menu

---
+++ src/model/resources/NamesListController.java
@@ -0,0 +1,62 @@
+package model.resources;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.input.MouseEvent;
+
+public class NamesListController {
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private Button setDefaultBtn;
+
+    @FXML
+    private Label defaultLabel;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private ListView<?> nameListView;
+
+    @FXML
+    private Label nameName;
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private Button ratingsButton;
+
+    @FXML
+    void audioRatingsPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backBtnPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playButtonClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ratingsButtonClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void setDefaultClicked(MouseEvent event) {
+
+    }
+
+}
