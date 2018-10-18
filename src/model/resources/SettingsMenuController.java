490840d8e201895cbd8284e844978f500e9fb336
rowanh22 <rowanhart22@gmail.com>
2018-10-19 11:36:45 +1300

    Light CSS changes & working add DB via settings

--- src/model/resources/SettingsMenuController.java
+++ src/model/resources/SettingsMenuController.java
@@ -3,6 +3,7 @@ package model.resources;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +11,18 @@ import java.io.IOException;
 public class SettingsMenuController {
 
     @FXML private Button backBtn;
+    @FXML private ComboBox<String> chooseDB;
 
     public void backBtnPressed() throws IOException {
         Scene scene = SetUp.getInstance().startMenu;
         Stage window = (Stage) backBtn.getScene().getWindow();
         window.setScene(scene);
+    }
+
+    public void initialize() {
+        chooseDB.getItems().add("Default Database");
+        chooseDB.getItems().add("Add new...");
+        chooseDB.getSelectionModel().selectFirst();
     }
 
 }
