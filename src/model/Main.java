b247e9678ca206f69c01fd0a45fc4780ca003d6d
rowanh22 <rowanhart22@gmail.com>
2018-10-18 23:28:26 +1300

    Refactored SetUp to reduce FXML loading, added check for default database presence.

--- src/model/Main.java
+++ src/model/Main.java
@@ -11,8 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Scene scene = SetUp.getInstance().startMenu; // load menu scene
+        SetUp.getInstance().startMenuController.setUp();
         primaryStage.setTitle("Name Sayer");
-        primaryStage.getIcons().add(new Image("https://i.imgur.com/qxLVZ0s.png"));
+        //TODO: make work with no internet connections
+        primaryStage.getIcons().add(new Image("/model/resources/images/icon.png"));
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
--- src/model/resources/StartMenuController.java
+++ src/model/resources/StartMenuController.java
@@ -1,12 +1,19 @@
 package model.resources;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class StartMenuController {
 
@@ -14,6 +21,39 @@ public class StartMenuController {
     @FXML private Button ratingsButton;
     @FXML private Button startButton;
     @FXML private Button instructionsButton;
+    private Stage stage = new Stage();
+
+    //TODO check the name folder is in right place
+
+    public void setUp() {
+        File f = new File(System.getProperty("user.dir") + "/names");
+        if(!f.exists() || !f.isDirectory()) {
+            BorderPane pane = new BorderPane();
+            pane.setStyle("-fx-background-image: none");
+            Label infoLabel = new Label("You will not be able to use Name Sayer without the default database, a folder called 'names' in the same directory as the jar file. To fix this, move the names databse to the correct location or add a new database from the settings menu.");
+            infoLabel.setWrapText(true);
+            Label titleLabel = new Label("Default Database Missing!");
+            titleLabel.setStyle("-fx-font-weight: bold; -fx-font-size: 19");
+            Button okButton = new Button("OK");
+
+            okButton.setOnAction(e -> stage.close());
+
+            pane.setCenter(infoLabel);
+            pane.setTop(titleLabel);
+            pane.setBottom(okButton);
+            pane.setMaxSize(400, 150);
+            pane.setMinSize(400, 150);
+
+            Scene scene = new Scene(pane);
+            scene.getStylesheets().add("/model/resources/Theme.css");
+            stage.setScene(scene);
+            stage.getIcons().add(new Image("/model/resources/images/icon.png"));
+            stage.setTitle("Database not present");
+            stage.setResizable(false);
+            stage.setAlwaysOnTop(true);
+            stage.show();
+        }
+    }
 
     @FXML
     void instructionsButtonClicked() throws IOException {
