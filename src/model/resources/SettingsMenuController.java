3c02a29391145d6b84a5ced19e2d288af80d0f50
rowanh22 <rowanhart22@gmail.com>
2018-10-19 22:51:21 +1300

    Lowered my standards with regard to the settings menu functionality

--- src/model/resources/SettingsMenuController.java
+++ src/model/resources/SettingsMenuController.java
@@ -15,6 +15,7 @@ import model.DatabaseProcessor;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
